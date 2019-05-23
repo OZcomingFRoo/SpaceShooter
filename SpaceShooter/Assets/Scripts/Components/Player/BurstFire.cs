7614822b70b097fd9ca8a073961b0ca06780a43f
OZcomingFRoo <OZcomingFRoo@gmail.com>
2019-05-23 12:33:37 +0300

    Game Developed Part 2
     - Implamented Burst and single laser shots
     - Remove Laser beam shot

--- SpaceShooter/Assets/Scripts/Components/Player/BurstFire.cs
+++ SpaceShooter/Assets/Scripts/Components/Player/BurstFire.cs
@@ -4,25 +4,47 @@ using UnityEngine;
 
 public class BurstFire : MonoBehaviour
 {
+    #region SerializeField
     [SerializeField]
     GameObject LaserPrefab;
+
     [SerializeField]
     float LaserSpeed;
+
     [SerializeField]
     float LaserDamage;
+
+    [SerializeField]
+    [Range(2,4)]
+    byte numberOfBurstShots;
+
+    [Range(0.0001f,0.3f)]
+    [SerializeField]
+    float burstFireRate;
+    #endregion
 
     void Start()
     {
         if (LaserSpeed <= 0) LaserSpeed = 3;
+        if (numberOfBurstShots <= 1) numberOfBurstShots = 3;
+        if (burstFireRate <= 0) burstFireRate = 0.1f;
     }
 
     void Update()
     {
         if (Input.GetButtonDown(AxesUtils.BurstFire))
         {
-            print(AxesUtils.BurstFire);
+            StartCoroutine(BurstShots());
+        }
+    }
+
+    IEnumerator BurstShots()
+    {
+        for (int i = 0; i < numberOfBurstShots; i++)
+        {
             var Laser = Instantiate(LaserPrefab, this.transform.position, Quaternion.identity);
             Laser.GetComponent<Rigidbody2D>().velocity = Vector2.up * LaserSpeed;
+            yield return new WaitForSeconds(burstFireRate);
         }
     }
 }
--- SpaceShooter/Assets/Scripts/Components/Player/DoubleShot.cs
+++ SpaceShooter/Assets/Scripts/Components/Player/DoubleShot.cs
@@ -4,26 +4,54 @@ using UnityEngine;
 
 public class DoubleShot : MonoBehaviour
 {
+    #region SerializeFields
     [SerializeField]
     GameObject LaserPrefab;
     [SerializeField]
     float LaserSpeed;
     [SerializeField]
     float LaserDamage;
+    [SerializeField]
+    [Range(0.001f, 1)]
+    float LaserFireRate; 
+    #endregion
+
+    public float LaserFireRateCoroutine { get { return LaserFireRate; } }
+    private List<Coroutine> FireCorotines { get; set; }
 
     void Start()
     {
         if (LaserSpeed <= 0) LaserSpeed = 3;
+        if (LaserDamage <= 0) LaserDamage = 2;
+        FireCorotines = new List<Coroutine>();
     }
 
     void Update()
     {
         if (Input.GetButtonDown(AxesUtils.DoubleShot))
         {
-            print(AxesUtils.DoubleShot);
-            var Laser = Instantiate(LaserPrefab, this.transform.position, Quaternion.identity);
-            Laser.GetComponent<Rigidbody2D>().velocity = Vector2.up * LaserSpeed;
+            FireCorotines.Add(StartCoroutine(Fire()));
+        }
+        else if (Input.GetButtonUp(AxesUtils.DoubleShot))
+        {
+            FireCorotines.ForEach(fireCoroutine => StopCoroutine(fireCoroutine));
+        }
+    }
 
+    IEnumerator Fire()
+    {
+        while (true)
+        {
+            // Spawn 2 lasers
+            var Laser1 = Instantiate(LaserPrefab, this.transform.position, Quaternion.identity);
+            var Laser2 = Instantiate(LaserPrefab, this.transform.position, Quaternion.identity);
+            // Set lasers position side by side
+            Laser1.transform.position = new Vector2(Laser1.transform.position.x - 0.5f, Laser1.transform.position.y);
+            Laser2.transform.position = new Vector2(Laser2.transform.position.x + 0.5f, Laser2.transform.position.y);
+            // Shoot 2 lasers
+            Laser1.GetComponent<Rigidbody2D>().velocity = Vector2.up * LaserSpeed;
+            Laser2.GetComponent<Rigidbody2D>().velocity = Vector2.up * LaserSpeed;
+            yield return new WaitForSeconds(this.LaserFireRateCoroutine);
         }
     }
 }
