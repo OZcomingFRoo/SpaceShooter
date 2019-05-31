518cdd8f24a61a9aa568c82ccf4727556a52add1
OZcomingFRoo <OZcomingFRoo@gmail.com>
2019-05-31 12:28:16 +0300

    Added rigidbody to moving objects part 1

---
+++ SpaceShooter/Assets/Scripts/Components/Shared/HealthComponent.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    [SerializeField]
+    private float health;
+
+    /// <summary>
+    /// Health that is set by laser shots
+    /// </summary>
+    public float Health { get; set; }
+
+    public event Action BeforeObjectDies;
+    public event Action OnObjectTookDamage;
+
+    void Start()
+    {
+        Health = health;
+        if (BeforeObjectDies == null) BeforeObjectDies = () => { Debug.Log("Object " + gameObject.name + " is dead"); };
+        if (OnObjectTookDamage == null) OnObjectTookDamage = () => { Debug.Log("Object " + gameObject.name + " Too damage"); };
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Calculate damage
+        if (collision.GetComponent<LaserComponent>() != null)
+        {
+            Health -= collision.GetComponent<LaserComponent>().LaserDamage;
+        }
+        else if(gameObject.tag == "Player")
+        {
+            Health--;
+        }
+        // Invoke the relevent event
+        if(Health <= 0)
+        {
+            BeforeObjectDies.Invoke();
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            OnObjectTookDamage.Invoke();
+        }
+    }
+}
---
+++ SpaceShooter/Assets/Scripts/Components/Shared/LaserComponent.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class LaserComponent : MonoBehaviour
+{
+    [SerializeField]
+    [Min(0.000000001f)]
+    float laserDamage;
+
+    public float LaserDamage { get; set; }
+
+    void Start()
+    {
+        if (LaserDamage <= 0) LaserDamage = this.laserDamage;
+    }
+}
