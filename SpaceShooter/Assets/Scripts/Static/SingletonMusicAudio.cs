2896b59a340b129a91518a8d5db6f402ae2a5686
OZcomingFRoo <OZcomingFRoo@gmail.com>
2019-06-28 13:08:45 +0300

    Added music to sum stages

---
+++ SpaceShooter/Assets/Scripts/Static/SingletonMusicAudio.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class SingletonMusicAudio : MonoBehaviour
+{
+    [SerializeField]
+    static bool staticFuncActicated = false;
+    void Awake()
+    {
+        SetSingleton();
+    }
+
+    private static void SetSingleton()
+    {
+        var audios = FindObjectsOfType<SingletonMusicAudio>();
+        if (audios.Length > 1)
+        {
+            for (int i = 1; i < audios.Length; i++)
+            {
+                Destroy(audios[i].gameObject);
+            }
+        }
+        DontDestroyOnLoad(audios[0]);
+    }
+}
